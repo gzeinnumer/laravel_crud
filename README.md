839f3ee82209e4bc692d525cf0dc053aab70cfaf
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-06-14 10:47:06 +0700

    commit

--- README.md
+++ README.md
@@ -570,6 +570,31 @@ class SiswaController extends Controller
 http://localhost:8081/siswa
 ```
 
+---
+#### PART 11
+* laravel_crud\app\Http\Controllers\SiswaController.php
+```php
+<?php
+class SiswaController extends Controller
+{
+    public function index(Request $r)
+    {
+        if($r->has('cari')){
+            $data_siswa = SiswaModel::where('nama_depan','LIKE','%'.$r->cari.'%')->get();
+        } else{
+            $data_siswa = SiswaModel::all();
+        }
+        return view('siswa.index',['data_siswa'=>$data_siswa]);
+    }
+}
+```
+```
+http://localhost:8081/siswa?cari=R
+```
+
+
+
+
 
 ---
 
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -21,8 +21,8 @@
                             <a class="nav-link" href="/siswa">Siswa</a>
                         </li>
                     </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <form class="d-flex" method="get" action="/siswa">
+                        <input name="cari" class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
                 </div>
